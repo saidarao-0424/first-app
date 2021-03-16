e48633a406efe136af3c35b67ba89107ed2eeac9
saidarao <saidarao.kotari@gmail.com>
2021-03-16 10:52:18 +0800

    Add

--- src/main/java/com/saida/sample/firstapp/controller/CustomerApi.java
+++ src/main/java/com/saida/sample/firstapp/controller/CustomerApi.java
@@ -2,11 +2,14 @@ package com.saida.sample.firstapp.controller;
 
 import com.saida.sample.firstapp.model.Customer;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,28 +17,34 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/api/customers")
+@RequestMapping("/api/v1/customers")
 @Tag(name = "Customer API", description = "the Customer API")
+@SecurityScheme(
+        name = "basicAuth", // can be set to anything
+        type = SecuritySchemeType.HTTP,
+        scheme = "basic"
+)
 public interface CustomerApi {
 
     @PostMapping(path = "/create", consumes = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(summary = "Creates Customer with given body")
+    @Operation(summary = "Creates Customer with given body", security = @SecurityRequirement(name = "basicAuth"))
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Customer Created successfully",
                     content = {@Content(mediaType = "application/json",
                             schema = @Schema(implementation = Customer.class))})})
-    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) ;
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer);
 
 
     @PutMapping(path = "/update", consumes = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(summary = "Updates Customer with given body")
+    @Operation(summary = "Updates Customer with given body", security = @SecurityRequirement(name = "basicAuth"))
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Customer Updated successfully",
                     content = {@Content(mediaType = "application/json",
                             schema = @Schema(implementation = Customer.class))})})
-    public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer) ;
+    public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer);
+
     @DeleteMapping(path = "/delete/{id}")
-    @Operation(summary = "Deletes Customer with given id")
+    @Operation(summary = "Deletes Customer with given id", security = @SecurityRequirement(name = "basicAuth"))
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Customer Deleted successfully",
                     content = {@Content(mediaType = "application/json",
@@ -43,7 +52,7 @@ public interface CustomerApi {
     public ResponseEntity<Void> deleteCustomer(@PathVariable Integer id);
 
     @GetMapping(path = "/find/{id}")
-    @Operation(summary = "Get a book by its id")
+    @Operation(summary = "Get a book by its id", security = @SecurityRequirement(name = "basicAuth"))
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Found the Customer",
                     content = {@Content(mediaType = "application/json",
@@ -52,10 +61,10 @@ public interface CustomerApi {
                     content = @Content),
             @ApiResponse(responseCode = "404", description = "Customer not found",
                     content = @Content)})
-    public Customer getCustomer(@PathVariable Integer id) ;
+    public Customer getCustomer(@PathVariable Integer id);
 
     @GetMapping(path = "/list")
-    @Operation(summary = "List all Customers")
+    @Operation(summary = "List all Customers", security = @SecurityRequirement(name = "basicAuth"))
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "successful operation",
                     content = @Content(array = @ArraySchema(schema = @Schema(implementation = Customer.class))))})
---
+++ src/main/java/com/saida/sample/firstapp/security/SecurityConfiguration.java
@@ -0,0 +1,60 @@
+package com.saida.sample.firstapp.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfiguration(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //To disable csrf cors configuration which is enabled by default with spring security
+        http.cors().and().csrf().disable().
+                authorizeRequests()
+                .antMatchers("/", "/index", "/css/*", "/js/*").permitAll()
+                .anyRequest().authenticated().and().httpBasic();
+
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails securityCredentials = User.builder().username("saida").password(passwordEncoder.encode("password")).roles("student").build();
+        return new InMemoryUserDetailsManager(securityCredentials);
+    }
+}
+
